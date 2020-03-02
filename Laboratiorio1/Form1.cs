e230cd216eb4c658506ba56f7ad2de3ed573b217
Your Name <juniorvilchezramos12@gmail.com>
2020-03-02 18:34:58 -0500

    first commit

---
+++ Laboratiorio1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratiorio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'neptunoDataSet.clientes' Puede moverla o quitarla según sea necesario.
+            this.clientesTableAdapter.Fill(this.neptunoDataSet.clientes);
+            lblTotal.Text = dgClientes.RowCount.ToString();
+
+        }
+    }
+}
